f0256c67697e4211ea08670810248105f36dd18e
NaijaTechBro <bakisodiq@gmail.com>
2023-05-02 09:02:05 +0100

    swagger UI

--- routes/root.js
+++ routes/root.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const path = require('path')
 
 router.get('^/$|/index(.html)?', (req, res) => {
-    res.send('<h1>Redox API</h1><a href="/api-docs">Documentation</a>');
+    res.send('<h1>Redox API</h1><a href="/api/v1/api-docs">Documentation</a>');
     // res.sendFile(path.join(__dirname, '..', 'views', 'index.html'))
 })
 
