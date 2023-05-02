b80f24b93cf82432f21f35980a23393525e3a4b9
NaijaTechBro <bakisodiq@gmail.com>
2023-05-02 09:17:26 +0100

    swagger UI

--- app.js
+++ app.js
@@ -44,6 +44,18 @@ app.use(errorHandler)
 // Cross Origin Resource Sharing
 app.use(cors(corsOptions));
 
+app.use((req, res, next,) => {
+    res.set({
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
+        "Access-Control-Allow-Methods": "GET, POST, PATCH, DELETE, OPTIONS",
+        "Content-Security-Policy": "default-src *",
+        "X-Content-Security-Policy": "default-src *",
+        "X-WebKit-CSP": "default-src *"
+    })
+    next();
+});
+
 app.use(express.json({ limit: "30mb", extended: true}))
 app.use(helmet());
 app.use(xss());
