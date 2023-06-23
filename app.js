209d199fe3be3f68979f5623b8a861c63a976f42
NaijaTechBro <bakisodiq@gmail.com>
2023-06-23 22:55:44 +0100

    app

--- app.js
+++ app.js
@@ -5,16 +5,26 @@ const chalk = require('chalk');
 
 // Security
 const helmet = require('helmet');
+const morgan = require("morgan");
+const mongoSanitize = require('express-mongo-sanitize');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+
 const cors = require('cors');
 const xss = require('xss-clean');
+const app = express();
+
+// Setting up socket.io config
+const http = require('http');
+const server  = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
 
 // Swagger UI
 const swaggerUI = require("swagger-ui-express");
 const YAML = require("yamljs");
 const swaggerJsDocs = YAML.load("./api.yaml");
 
-const app = express();
-const http = require('http');
 const path = require('path');
 const { logger, logEvents } = require('./middleware/logger');
 const errorHandler = require('./middleware/errorHandler');
@@ -22,7 +32,7 @@ const corsOptions = require('./config/corsOptions');
 const bodyParser = require('body-parser');
 const cookieParser = require("cookie-parser");
 
-const server  = http.createServer(app);
+
 
 const connectDB = require('./config/dbConn');
 
@@ -40,7 +50,7 @@ connectDB()
 // Middlewares
 app.use(logger)
 app.use(errorHandler)
-
+app.use(morgan('dev'));
 
 // Cross Origin Resource Sharing
 app.use(cors(corsOptions));
@@ -58,12 +68,34 @@ app.use((req, res, next,) => {
 });
 
 app.use(express.json({ limit: "30mb", extended: true}))
-app.use(helmet());
+app.use(
+    helmet.contentSecurityPolicy({
+        useDefaults: true,
+        directives: {
+            "img-src": ["'self'", "https: data:"]
+        }
+    })
+    );
 app.use(xss());
 app.use(cookieParser())
 app.use(express.urlencoded({ limit: "30mb", extended: false}))
-app.use(bodyParser.json())
+app.use(bodyParser.json({ limit: "5mb" }))
 
+// Prevent SQL injection
+app.use(mongoSanitize());
+
+// limit queries per 15min
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
+app.use(limiter);
+
+// HTTP Param Pollution
+app.use(hpp());
 
 // Routes Middleware
 app.use("/api/v1/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerJsDocs));
@@ -86,8 +118,15 @@ app.all('*', (req, res) => {
         res.type('txt').send('404 Not Found')
     }
 })
-// Error Middleware
 
 
+// Use Socket io
+io.on('connection', (socket) => {
+    socket.on('comment', (msg) => {
+        io.emit("new-comment", msg);
+    })
+})
+
+exports.io = io
 
 module.exports = app;
