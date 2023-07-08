d0484fa56b89fa629749b6f03654972bed7fc14a
NaijaTechBro <bakisodiq@gmail.com>
2023-07-08 10:05:34 +0100

    auth

--- routes/admin/authRoutes.js
+++ routes/admin/authRoutes.js
@@ -16,28 +16,22 @@ const {
     sendLoginCode,
     loginWithCode,
 } = require("../../controllers/admin/authControllers");
-const {
-    isAuthenticatedUser,
-} = require("../../middleware/authMiddleware");
-const loginLimiter = require("../../middleware/loginLimiter");
+
 
 router.post("/auth/register", registerAdmin);
-router.post("/auth/sendVerificationEmail", sendVerificationEmail);
-router.patch("/auth/verifyUser/:verificationToken", verifyUser);
-router.post("/auth/login", loginLimiter, loginAdmin);
+router.post("/auth/login", loginAdmin);
 router.get("/logout", logout);
-
-router.post("/sendAutomatedEmail", sendAutomatedEmail);
-router.post("/sendLoginCode/:email", sendLoginCode);
-router.post("/loginWithCode/:email", loginWithCode);
-
 router.get("/auth/loginStatus", loginStatus);
 router.patch("/updateUser", updateUser);
-router.patch("/changePassword", changePassword);
 router.post("/forgotPassword", forgotPassword);
 router.patch("/resetPassword/:resetToken", resetPassword);
-
-router.post("/auth/google/callback/:ID", loginLimiter, loginWithGoogle);
+// router.post("/auth/sendVerificationEmail", sendVerificationEmail);
+// router.patch("/auth/verifyUser/:verificationToken", verifyUser);
+// router.post("/sendAutomatedEmail", sendAutomatedEmail);
+// router.post("/sendLoginCode/:email", sendLoginCode);
+// router.post("/loginWithCode/:email", loginWithCode);
+// router.patch("/changePassword", changePassword);
+// router.post("/auth/google/callback/:ID", loginWithGoogle);
 
 
 module.exports = router;
